138bb2a0bc62b3851d13f33e7e8d41cfa6bcb813
hbrls <shuaizhexu@gmail.com>
2026-09-08 16:12:58 +0800

    [nice] 清理 reposlide 和 repowiki 相关功能

--- src/app/workspace/[workspaceId]/kanban/__tests__/kanban-tab-modals.test.tsx
+++ src/app/workspace/[workspaceId]/kanban/__tests__/kanban-tab-modals.test.tsx
@@ -306,7 +306,6 @@ describe("KanbanCodebaseModal", () => {
     expect(sourceTypeField).toBeTruthy();
 
     expect(screen.queryByRole("button", { name: /phodal\/routa/ })).toBeNull();
-    expect(screen.getByRole("link", { name: /Open RepoSlide|打开 RepoSlide/ })).toBeTruthy();
     expect(screen.getByRole("button", { name: /Edit|编辑/ })).toBeTruthy();
     expect(screen.getByRole("button", { name: /Remove|移除/ })).toBeTruthy();
     expect(screen.getByRole("button", { name: /Close|关闭/ })).toBeTruthy();
